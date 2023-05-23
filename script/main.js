e633d32b4d49e0b4772dd54c8864bf998fcc20b7
Sky2Walker <starw8169@gmail.com>
2023-05-23 19:57:16 +0300

    slider and usage

---
+++ script/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows:true,
+        dots:true,
+        autoplay:true,
+        autoplaySpeed:3000,
+        infinite:true,
+    });
+});
